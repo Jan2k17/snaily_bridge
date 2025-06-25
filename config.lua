234b5f5260821e51d84bc012c1500740a0e89f76
Jan2k17 <56897434+Jan2k17@users.noreply.github.com>
2025-06-25 21:08:14 +0200

    -/-

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Snaily = {
-    API_URL = "https://cad-api.wcc-rp.de/v1/",
-    API_TOKEN = "sng_OOsMeH-o96JQG3Fxyo-Q8hkyxnrfqyKBZtiPu2kTrhOPkk0rgCmVlZro",
-    API_TOKEN_HEADER_NAME = "snaily-cad-api-token"
+    API_URL = "***",
+    API_TOKEN = "***",
+    API_TOKEN_HEADER_NAME = "snaily-cad-api-token" -- DO NOT TOUCH
 }
