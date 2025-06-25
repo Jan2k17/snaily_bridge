4df9db329ba5a4dd6e7829c39cf7d084648a1b5c
Jan2k17 <56897434+Jan2k17@users.noreply.github.com>
2025-06-25 21:10:30 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Snaily = {
-    API_URL = "***",
-    API_TOKEN = "***",
-    API_TOKEN_HEADER_NAME = "snaily-cad-api-token" -- DO NOT TOUCH
+    API_URL = GetConvar("snailycad_v4_api_url"),
+    API_TOKEN = GetConvar("snailycad_v4_api_token"),
+    API_TOKEN_HEADER_NAME = "snaily-cad-api-token"
 }
